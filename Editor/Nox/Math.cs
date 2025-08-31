4dba2bc8d0ad7c9355612b1d7e884c6bc47f0aa6
nitro <38311969+noxitro@users.noreply.github.com>
2025-09-01 02:49:01 +0900

    editor追加

---
+++ Editor/Nox/Math.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Nox
+{
+	file interface Point2D<T> where T : struct, System.Numerics.INumber<T>
+	{
+		public T x { get; set; }
+		public T y { get; set; }
+
+	}
+
+	public struct Float2 : Point2D<float>
+	{
+		public float x { readonly get; set; }
+		public float y { readonly get; set; }
+	}
+
+	public struct Float2Legacy
+	{
+	}
+}
---
+++ Editor/Nox/Singleton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nox
+{
+	public interface ISingleton<T>
+#if DEBUG
+		: IDisposable
+#endif
+		where T : class, new()
+	{
+		#region 公開
+		public static T Instance
+		{
+			get
+			{
+				System.Diagnostics.Debug.Assert(_Instance != null, "Singleton instance not initialized. Call Initialize() first.");
+				return _Instance;
+			}
+		}
+
+		public static bool HasInstance => _Instance != null;
+
+		public static void CreateInstance()
+		{
+			System.Diagnostics.Debug.Assert(_Instance == null, "Singleton instance already initialized.");
+			_Instance = new T();
+		}
+
+		public static void DeleteInstance()
+		{
+			System.Diagnostics.Debug.Assert(_Instance != null, "Singleton instance not initialized.");
+			_Instance = null;
+		}
+		#endregion
+
+		#region 非公開
+		private static T? _Instance = null;
+
+#if DEBUG
+		void IDisposable.Dispose()
+		{
+			System.Diagnostics.Debug.Assert(_Instance != null, "Singleton instance not initialized.");
+			_Instance = null;
+		}
+#endif
+		#endregion
+	}
+
+	public class Runtime : ISingleton<Runtime>
+	{
+		public void test()
+		{
+		}
+	}
+
+
+}
