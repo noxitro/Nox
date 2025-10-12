5de7a74c384704625c611e7e689c787466367702
nitro <38311969+noxitro@users.noreply.github.com>
2025-10-12 14:35:30 +0900

    不要ファイル削除

--- Editor/Studio/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Studio")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Studio.Pages")]
