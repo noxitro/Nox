e051bcfe257551d1801b2bee9b401b839ee0de15
nitro <38311969+noxitro@users.noreply.github.com>
2026-09-25 19:36:49 +0900

    nox_module: リフレクション生成器が include する <モジュール名>.h をテンプレートに含める
    Entry.cs は slnx 上の各プロジェクトについて <プロジェクト名>.h を生成コードから include する。
    テンプレートに無かったため hid を再生成した時点で reflection_generated が C1083 で落ちた。

---
+++ runtime/modules/hid/hid.h
@@ -0,0 +1,7 @@
+﻿//	Copyright (c) 2023-2026 noxitro
+//	SPDX-License-Identifier: MIT
+
+///	@file	hid.h
+///	@brief	hid
+///	@note	リフレクション生成器 (ReflectionGenerator) が生成コードから include する。削除・改名しないこと
+#pragma once
